bf07adda9091960c67ec95c814436fb4ff6724b2
黄保贤 <huangbaoxian@edaijia-inc.cn>
2017-12-22 18:42:37 +0800

    添加多线操作

---
+++ iOSTheRoadToTheAdvanced/theadController/model/BaoXianOperation.h
@@ -0,0 +1,13 @@
+//
+//  BaoXianOperation.h
+//  iOSTheRoadToTheAdvanced
+//
+//  Created by 黄保贤 on 2017/12/22.
+//  Copyright © 2017年 黄保贤. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BaoXianOperation : NSOperation
+
+@end
---
+++ iOSTheRoadToTheAdvanced/theadController/model/BaoXianOperation.m
@@ -0,0 +1,25 @@
+//
+//  BaoXianOperation.m
+//  iOSTheRoadToTheAdvanced
+//
+//  Created by 黄保贤 on 2017/12/22.
+//  Copyright © 2017年 黄保贤. All rights reserved.
+//
+
+#import "BaoXianOperation.h"
+
+@implementation BaoXianOperation
+
+
+- (void)main {
+    
+    NSLog(@" BaoXianOperation  main log %@", [NSThread currentThread]);
+    
+}
+
+-(void)dealloc {
+    
+    NSLog(@" BaoXianOperation dealloc log");
+}
+
+@end
