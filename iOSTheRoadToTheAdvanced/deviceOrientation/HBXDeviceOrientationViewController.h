75aa7aad26703b517fe839ffd00db53c83a68d02
huangbx <huangbx@example.com>
2018-08-20 17:57:29 +0800

    添加陀螺仪

---
+++ iOSTheRoadToTheAdvanced/deviceOrientation/HBXDeviceOrientationViewController.h
@@ -0,0 +1,13 @@
+//
+//  HBXDeviceOrientationViewController.h
+//  iOSTheRoadToTheAdvanced
+//
+//  Created by huangbaoxian on 2018/8/20.
+//  Copyright © 2018年 黄保贤. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HBXDeviceOrientationViewController : UIViewController
+
+@end
---
+++ iOSTheRoadToTheAdvanced/deviceOrientation/HBXDeviceOrientationViewController.m
@@ -0,0 +1,88 @@
+//
+//  HBXDeviceOrientationViewController.m
+//  iOSTheRoadToTheAdvanced
+//
+//  Created by huangbaoxian on 2018/8/20.
+//  Copyright © 2018年 黄保贤. All rights reserved.
+//
+
+#import "HBXDeviceOrientationViewController.h"
+#import "DeviceOrientation.h"
+
+@interface HBXDeviceOrientationViewController ()<DeviceOrientationDelegate>
+
+@property (nonatomic, strong) DeviceOrientation *origintation;
+
+@property (nonatomic, strong) UILabel *textLabel;
+
+
+@end
+
+@implementation HBXDeviceOrientationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.origintation = [[DeviceOrientation alloc] initWithDelegate:self];
+    self.view.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:self.textLabel];    
+    
+    // Do any additional setup after loading the view.
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self.origintation startMonitor];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    [self.origintation stop];
+}
+
+- (void)directionChange:(TgDirection)direction {
+    switch (direction) {
+        case TgDirectionDown:
+            self.textLabel.text = @"倒立竖屏";
+            break;
+        case TgDirectionleft:
+            self.textLabel.text = @"横屏向左";
+            break;
+        case TgDirectionRight:
+            self.textLabel.text = @"横屏向右";
+            break;
+        case TgDirectionPortrait:
+            self.textLabel.text = @"竖屏直列";
+            break;
+        default:
+            break;
+    }
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (UILabel *)textLabel {
+    if (!_textLabel) {
+        _textLabel = [[UILabel alloc] initWithFrame: CGRectMake(0, 200, self.view.frame.size.width, 100)];
+        _textLabel.textAlignment = NSTextAlignmentCenter;
+        _textLabel.textColor = [UIColor blackColor];
+    }
+    return _textLabel;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
