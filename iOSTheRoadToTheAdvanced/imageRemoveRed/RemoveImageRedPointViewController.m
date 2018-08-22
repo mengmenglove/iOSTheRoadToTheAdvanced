8e365d38526888b858b564103671ef93b2294890
huangbx <huangbx@example.com>
2018-08-22 14:47:42 +0800

    新增逻辑

--- iOSTheRoadToTheAdvanced/imageRemoveRed/RemoveImageRedPointViewController.m
+++ iOSTheRoadToTheAdvanced/imageRemoveRed/RemoveImageRedPointViewController.m
@@ -8,7 +8,7 @@
 
 #import "RemoveImageRedPointViewController.h"
 
-@interface RemoveImageRedPointViewController ()
+@interface RemoveImageRedPointViewController () <UIImagePickerControllerDelegate,UINavigationControllerDelegate>
 
 
 @property (nonatomic, strong) UIImage *image;
@@ -23,17 +23,38 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self.view addSubview:self.showImageView];
+    CGRect frame = self.showImageView.frame;
+    frame.origin = CGPointMake(0, 64);
+    self.showImageView.frame = frame;
+    
     self.image = [UIImage imageNamed:@"1532946340216"];
     
     UIBarButtonItem *rigthItem = [[UIBarButtonItem alloc] initWithTitle:@"去红"
                                                                   style:UIBarButtonItemStyleDone
                                                                  target:self
-                                                                 action:@selector(removeRedClick)];
-    self.navigationItem.rightBarButtonItem = rigthItem;
+                                                                 action:@selector(blackAndWhite)];
+    
+    
+    UIBarButtonItem *tokePhotoItem = [[UIBarButtonItem alloc] initWithTitle:@"拍照"
+                                                                  style:UIBarButtonItemStyleDone
+                                                                 target:self
+                                                                 action:@selector(takePhoto)];
+    
+    self.navigationItem.rightBarButtonItems = @[rigthItem , tokePhotoItem];
     
     self.showImageView.image = self.image;
     
     // Do any additional setup after loading the view.
+}
+
+- (void)takePhoto {
+    UIImagePickerController *imagePickerController = [[UIImagePickerController alloc] init];
+    imagePickerController.delegate = self;
+    //    imagePickerController.allowsEditing = YES;
+    imagePickerController.sourceType = UIImagePickerControllerSourceTypeCamera;
+    [self presentViewController:imagePickerController animated:true completion:^{
+        
+    }];
 }
 
 - (void)removeRedClick {
@@ -44,6 +65,85 @@
 }
 
 
+- (void)blackAndWhite {
+    __weak typeof(self) weakSelf = self;
+    [self drawImage:150  image:self.image  complete:^(UIImage *resultImage) {
+        weakSelf.showImageView.image  = resultImage;
+    }];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info{
+    UIImage *image = [info objectForKey:UIImagePickerControllerOriginalImage];
+    self.image = image;
+    self.showImageView.image = image;
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker{
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+- (void)drawImage:(double)filterValue image:(UIImage *)handleImage  complete:(handleImageCompleteBlock)complete
+{
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSLog(@"开始处理");
+        UIImage *image = handleImage;
+        // 分配内存
+        const int imageWidth = image.size.width;
+        const int imageHeight = image.size.height;
+        size_t      bytesPerRow = imageWidth * 4;
+        uint32_t* rgbImageBuf = (uint32_t*)malloc(bytesPerRow * imageHeight);
+        
+        // 创建context
+        CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+        CGContextRef context = CGBitmapContextCreate(rgbImageBuf, imageWidth, imageHeight, 8, bytesPerRow, colorSpace,kCGBitmapByteOrder32Little | kCGImageAlphaNoneSkipLast);
+        CGContextDrawImage(context, CGRectMake(0, 0, imageWidth, imageHeight), image.CGImage);
+        // 遍历像素
+        int pixelNum = imageWidth * imageHeight;
+        uint32_t* pCurPtr = rgbImageBuf;
+        
+        for (int i = 0; i < pixelNum; i++, pCurPtr++)
+        {
+            //      ABGR
+            uint8_t* ptr = (uint8_t*)pCurPtr;
+            int B = ptr[1];
+            int G = ptr[2];
+            int R = ptr[3];
+            double Gray = R*0.3+G*0.59+B*0.11;
+            if (Gray > filterValue || (Gray == filterValue && filterValue == 0)) {
+                ptr[0] = 255;
+                ptr[1] = 255;
+                ptr[2] = 255;
+            }else{
+                ptr[0] = 0;
+                ptr[1] = 0;
+                ptr[2] = 0;
+                //            ptr[3] = 0xff;
+            }
+        }
+        // 将内存转成image
+        CGDataProviderRef dataProvider = CGDataProviderCreateWithData(NULL, rgbImageBuf, bytesPerRow * imageHeight,NULL);
+        CGImageRef imageRef = CGImageCreate(imageWidth, imageHeight, 8, 32, bytesPerRow, colorSpace,kCGImageAlphaLast | kCGBitmapByteOrder32Little, dataProvider,NULL, true, kCGRenderingIntentDefault);
+        
+        CGDataProviderRelease(dataProvider);
+        
+        UIImage* resultUIImage = [UIImage imageWithCGImage:imageRef scale:image.scale orientation:image.imageOrientation];
+        // 释放
+        CGImageRelease(imageRef);
+        CGContextRelease(context);
+        CGColorSpaceRelease(colorSpace);
+        NSLog(@"处理完成");
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if (complete) {
+                complete(resultUIImage);
+            }
+        });
+    });
+   // self.outputImg.image = resultUIImage;
+}
+
+
+
 
 
 - (void)didReceiveMemoryWarning {
@@ -51,6 +151,11 @@
     // Dispose of any resources that can be recreated.
 }
 
+/***
+ 
+ 去红去蓝算法
+ 
+ **/
 
 #define Mask8(x) ( (x) & 0xFF )
 #define R(x) ( Mask8(x) )
