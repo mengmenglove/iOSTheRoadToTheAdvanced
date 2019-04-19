8cce14129f30feff9e679293cc869499bc5aa1bb
huangbx <huangbx@example.com>
2019-04-19 19:13:22 +0800

    1

--- iOSTheRoadToTheAdvanced/理论学习/controller/HBXFoundationViewController.m
+++ iOSTheRoadToTheAdvanced/理论学习/controller/HBXFoundationViewController.m
@@ -26,32 +26,159 @@
     [self.view addSubview:self.tableView];
     
     [self initData];
+    [self showExpress];
+    // Do any additional setup after loading the view.
+}
+
+- (void)fileWrite {
+    //文件追加
+//    NSString *homePath = NSHomeDirectory( );
+//    NSString *sourcePath = [NSString stringWithFormat:@"%@testfile.text", homePath] ;
+//    NSFileHandle *fielHandle = [NSFileHandle fileHandleForUpdatingAtPath:sourcePath];
+//    [fileHandle ];
+//    NSString *str = @"追加的数据";
+//    NSData* stringData  = [str dataUsingEncoding:NSUTF8StringEncoding];
+//    [fileHandle writeData:stringData]; 追加写入数据[fileHandle closeFile];
     
-    /*
-    NSString * str =  [NSByteCountFormatter stringFromByteCount:10929292 countStyle:NSByteCountFormatterCountStyleFile];
-    NSLog(@"str: %@", str);
-    "str: 10.9 MB"
-    */
-    /*
-    UInt16  Byte = 0x1234;
-    HTONS(Byte);//转换
-    NSLog(@"Byte == %x",Byte);
-    3412
-    */
+  
+}
+
+- (void)showExpress {
+    //1 做数学题  简单表达式
+    NSExpression *expression = [NSExpression expressionWithFormat:@"4 + 5 - 2**3"];
+    id value = [expression expressionValueWithObject:nil context:nil]; // => 1
+    NSLog(@"%@", value);
     
+    
+    
+    
+    NSArray *numbers = @[@(6), @(8)];
+    NSExpression *addexpression = [NSExpression expressionForFunction:@"max:" arguments:@[[NSExpression expressionForConstantValue:numbers]]];
     /*
-    NSString *str = @"今天我们来学习NSCharacterSet我们快乐";
-    NSString *str1 = @"我s今";
-    NSMutableString *resultStr = [[NSMutableString alloc]init];
-    for (int i = 0; i < str.length; i++) {
-        NSString *indexStr = [str substringWithRange:NSMakeRange(i, 1)];
-        if (![str1 containsString:indexStr]) {
-            [resultStr appendString:indexStr];
-        }
-    }
-    NSLog(@"%@",resultStr);
-    天们来学习NSCharacterSet们快乐
+     fucntion
+     average:
+     sum:
+     count:
+     min:
+     max:
+     median:
+     mode:
      */
+    value = [addexpression expressionValueWithObject:nil context:nil];
+    NSLog(@"%@", value);
+    
+    
+    /*
+     * 基本运算
+     add:to:
+     from:subtract:
+     multiply:by:
+     divide:by:
+     modulus:by:
+     abs:
+     
+     
+     */
+    
+    
+    /*
+     *高级运算
+     
+     sqrt:
+     log:
+     ln:
+     raise:toPower:
+     exp:
+
+     */
+    
+    
+    /*
+     * 随机函数
+     random
+     random:
+     */
+    
+    /*
+     * 二进制运算
+     bitwiseAnd:with:
+     bitwiseOr:with:
+     bitwiseXor:with:
+     leftshift:by:
+     rightshift:by:
+     onesComplement:
+     */
+    
+    
+    /*日期函数
+     *
+     now
+     */
+    
+    /*字符串函数
+     *
+     lowercase:
+     uppercase:
+     */
+    
+    
+    /*空操作
+     *
+     noindex:
+     */
+    
+    
+    /*自定义函数
+     
+     @interface NSNumber (Factorial)
+     - (NSNumber *)factorial;
+     @end
+     
+     @implementation NSNumber (Factorial)
+     - (NSNumber *)factorial {
+     return @(tgamma([self doubleValue] + 1));
+     }
+     @end
+     
+     NSExpression *expression = [NSExpression expressionWithFormat:@"FUNCTION(4.2, 'factorial')"];
+     id value = [expression expressionValueWithObject:nil context:nil]; // 32.578...
+     
+     *
+     */
+
+
+}
+
+- (void)showCharactor {
+
+    /*
+     NSString * str =  [NSByteCountFormatter stringFromByteCount:10929292 countStyle:NSByteCountFormatterCountStyleFile];
+     NSLog(@"str: %@", str);
+     "str: 10.9 MB"
+     */
+    
+     UInt16  Byte = 0x1234;
+     HTONS(Byte);//转换
+     NSLog(@"Byte == %x",Byte);
+    // 3412
+    
+    
+    
+     NSString *str = @"今天我们来学习NSCharacterSet我们快乐";
+     NSString *str1 = @"我s今";
+     NSMutableString *resultStr = [[NSMutableString alloc]init];
+     for (int i = 0; i < str.length; i++) {
+     NSString *indexStr = [str substringWithRange:NSMakeRange(i, 1)];
+     if (![str1 containsString:indexStr]) {
+     [resultStr appendString:indexStr];
+     }
+     }
+     NSLog(@"%@",resultStr);
+    
+    
+}
+
+- (void)showNSPredicate {
     
     //基础过滤
     NSMutableArray *array=[NSMutableArray array];//\n
@@ -59,38 +186,43 @@
     [array addObject:[[HBXLearnModel alloc] initWith:@"wy" age:20]];//\n
     [array addObject:[[HBXLearnModel alloc] initWith:@"LWW" age:21]];//\n
     [array addObject:[[HBXLearnModel alloc] initWith:@"sunshinelww" age:22]];//\n
-  
-//    NSPredicate *basicPredicate=[NSPredicate predicateWithFormat:@"name = 'lww'"];//\n
-//    //基础过滤//\n
-//    [array filterUsingPredicate: basicPredicate]; //通过条件表达式筛选数组元素//\n
+    
+    NSPredicate *basicPredicate=[NSPredicate predicateWithFormat:@"name = 'lww'"];//\n
+    //基础过滤//\n
+    [array filterUsingPredicate: basicPredicate]; //通过条件表达式筛选数组元素//\n
     
     NSMutableArray *predicateArray=[NSMutableArray array];//\n
     NSPredicate *basicPredicate1=[NSPredicate predicateWithFormat:@"name = 'lww'"];//\n
     NSPredicate *basicPredicate2=[NSPredicate predicateWithFormat:@"age = 20"];[predicateArray addObject: basicPredicate1];//\n
     [predicateArray addObject: basicPredicate2];//\n
     
-//    NSCompoundPredicate *orMatchPredicate=[NSCompoundPredicate orPredicateWithSubpredicates:predicateArray]; ///对//\n数组中的谓词表达式取或
-//    [array filterUsingPredicate:orMatchPredicate];//\n
-//    NSLog(@"array after: %@", array);//\n
+    //    NSCompoundPredicate *orMatchPredicate=[NSCompoundPredicate orPredicateWithSubpredicates:predicateArray]; ///对//\n数组中的谓词表达式取或
+    //    [array filterUsingPredicate:orMatchPredicate];//\n
+    //    NSLog(@"array after: %@", array);//\n
     
     
-//    NSCompoundPredicate *andMatchPredicate=[NSCompoundPredicate andPredicateWithSubpredicates:predicateArray];///对数组中的谓词表达式取与//\n
-//    [array filterUsingPredicate:andMatchPredicate];//\n
-//    NSLog(@"array after: %@", array);//\n
+    //    NSCompoundPredicate *andMatchPredicate=[NSCompoundPredicate andPredicateWithSubpredicates:predicateArray];///对数组中的谓词表达式取与//\n
+    //    [array filterUsingPredicate:andMatchPredicate];//\n
+    //    NSLog(@"array after: %@", array);//\n
     
     
-//    NSCompoundPredicate *noMatchPredicate = [NSCompoundPredicate notPredicateWithSubpredicate://\n basicPredicate1]; ///对basicPredicate1取反//\n
-//    [array filterUsingPredicate:noMatchPredicate];//\n
-//    NSLog(@"array after: %@", array);//\n
+    //    NSCompoundPredicate *noMatchPredicate = [NSCompoundPredicate notPredicateWithSubpredicate://\n basicPredicate1]; ///对basicPredicate1取反//\n
+    //    [array filterUsingPredicate:noMatchPredicate];//\n
+    //    NSLog(@"array after: %@", array);//\n
     
-    /*
+    
+}
+
+- (void)showEnumeratorAction {
     NSArray *newArray = @[@"1", @"2", @"3"];
     NSEnumerator *enumtrator = [newArray objectEnumerator];
     NSString *obj = nil;
     while (obj  = [enumtrator nextObject]) {
         NSLog(@"obj: %@", obj);
     }
-     */
+}
+
+- (void)getExceotionAction {
     //异常的名称
     NSString *exceptionName = @"自定义异常";
     //异常的原因
@@ -107,8 +239,6 @@
         @throw exception;
     }
     
-    
-    // Do any additional setup after loading the view.
 }
 
 - (NSString *)getTextWithDict:(NSDictionary *)dict {
